6d22af1522ef9d7f3946e32ab3194647af6c872b
Fortncom <anatoliy.gromov.service@gmail.com>
2024-10-20 21:51:38 +0300

    Переиспользование метода max() с 2 аргументами

--- src/main/kotlin/ru/job4j/base/Condition.kt
+++ src/main/kotlin/ru/job4j/base/Condition.kt
@@ -4,4 +4,4 @@ fun max(first: Int, second: Int) : Int
    = if (first > second) first else second
 
 fun max(first: Int, second: Int, third: Int) : Int
-= if (first > second && first > third) first else (if (second > third) second else third)
+   = max(max(first, second), third)
