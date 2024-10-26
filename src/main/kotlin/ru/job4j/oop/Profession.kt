a33efe6360ff16bb7960e24ee0025c71c9fbbadf
Fortncom <anatoliy.gromov.service@gmail.com>
2024-10-26 22:50:56 +0300

    Поменял название метода

--- src/main/kotlin/ru/job4j/oop/Profession.kt
+++ src/main/kotlin/ru/job4j/oop/Profession.kt
@@ -4,7 +4,7 @@ open class Profession(var name: String = "",
                       var surname: String = "",
                       var education: String = "") {
 
-    open fun getName() = name
+    open fun getFullName() = "%s %s".format(name, surname)
     open fun action() = "work"
 }
 
@@ -13,7 +13,7 @@ class Doctor(name: String,
              education: String,
              var diagnosis: String = "") : Profession(name, surname, education) {
 
-    override fun getName() = "Doctor's name: %s".format(name)
+    override fun getFullName() = "Doctor's name: %s %s".format(name, surname)
     override fun action(): String {
         return "action from doctor %s".format(super.action())
     }
@@ -25,7 +25,7 @@ class Engineer(name: String,
                education: String,
                var computer: String = "") : Profession(name, surname, education) {
 
-    override fun getName() = "Engineer's name: %s".format(name)
+    override fun getFullName() = "Engineer's name: %s %s".format(name, surname)
     override fun action(): String {
         return "action from engineer %s".format(super.action())
     }
