75547b1784ef820ed614ba19c3d95196697c4e79
Fortncom <anatoliy.gromov.service@gmail.com>
2024-10-26 22:30:53 +0300

    8. Переопределение. [#191436]

--- src/main/kotlin/ru/job4j/oop/Profession.kt
+++ src/main/kotlin/ru/job4j/oop/Profession.kt
@@ -2,16 +2,32 @@ package ru.job4j.oop
 
 open class Profession(var name: String = "",
                       var surname: String = "",
-                      var education: String = "")
+                      var education: String = "") {
+
+    open fun getName() = name
+    open fun action() = "work"
+}
 
 class Doctor(name: String,
              surname: String,
              education: String,
-             var diagnosis: String = "") : Profession(name, surname, education)
+             var diagnosis: String = "") : Profession(name, surname, education) {
+
+    override fun getName() = "Doctor's name: %s".format(name)
+    override fun action(): String {
+        return "action from doctor %s".format(super.action())
+    }
+}
 
 
 class Engineer(name: String,
                surname: String,
                education: String,
-               var computer: String = "") : Profession(name, surname, education)
+               var computer: String = "") : Profession(name, surname, education) {
+
+    override fun getName() = "Engineer's name: %s".format(name)
+    override fun action(): String {
+        return "action from engineer %s".format(super.action())
+    }
+}
 
