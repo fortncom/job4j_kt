37e6c50c043ee0510115f814e4a15295c076d459
Fortncom <anatoliy.gromov.service@gmail.com>
2024-10-29 00:24:08 +0300

    4. Последовательности [#338630]

--- src/main/kotlin/ru/job4j/lambda/Filter.kt
+++ src/main/kotlin/ru/job4j/lambda/Filter.kt
@@ -3,6 +3,15 @@ package ru.job4j.lambda
 val list = listOf(1, 2, 3, 4)
 val filtered = list.filter { it % 2 == 0 }
 
+fun count(list: ArrayList<Int>): Int? {
+    return list
+        .stream()
+        .filter { it % 2 == 0 }
+        .map { it + 1 }
+        .reduce(0, ) { acc, value -> acc + value }
+}
+
 fun main() {
     println(filtered)
+    println(count(ArrayList(list)))
 }
