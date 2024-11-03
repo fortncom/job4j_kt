835de624db6dd9e1989c77669831ca508b94ba4b
Fortncom <anatoliy.gromov.service@gmail.com>
2024-11-04 00:58:46 +0300

    Исправления к Map [#338629 #519145]

--- src/main/kotlin/ru/job4j/lambda/Campaign.kt
+++ src/main/kotlin/ru/job4j/lambda/Campaign.kt
@@ -2,7 +2,10 @@ package ru.job4j.lambda
 
 import java.util.*
 
-data class Campaign(var name: String, var address: Address, var created: Date) {
+data class Campaign(
+    var name: String = "",
+    var address: Address? = null,
+    var created: Date = Date()) {
 
     fun printCompaigns(compaigns: List<Campaign>) = compaigns.map { it.toString() }.toList()
 }
--- src/test/kotlin/ru/job4j/lambda/CampaignTest.kt
+++ src/test/kotlin/ru/job4j/lambda/CampaignTest.kt
@@ -30,10 +30,8 @@ class CampaignTest {
             )
         )
 
-        val rsl = campaigns.map { it.toString() }.toList()
-
-        val expected = listOf("Campaign(name=IT Systems, address=Address(city=Москва, street=Ленинградский проспект, house=7), created=Wed Nov 26 00:00:00 MSK 3924)",
-                "Campaign(name=TRS, address=Address(city=Санкт-Петербург, street=Невский проспект, house=12), created=Tue Nov 25 00:00:00 MSK 3924)")
+        val expected = campaigns.map { it.toString() }.toList()
+        val rsl = Campaign().printCompaigns(campaigns)
 
         assertThat(rsl).isEqualTo(expected)
     }
