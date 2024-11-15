89591700899101fe4cfd1f021487ae8bd90d02e8
Fortncom <anatoliy.gromov.service@gmail.com>
2024-11-15 23:33:46 +0300

    тренировочное задание

--- src/main/kotlin/ru/job4j/lambda/Filter.kt
+++ src/main/kotlin/ru/job4j/lambda/Filter.kt
@@ -1,5 +1,9 @@
 package ru.job4j.lambda
 
+import java.util.*
+import java.util.function.Function
+import java.util.stream.Collectors
+
 val list = listOf(1, 2, 3, 4)
 val filtered = list.filter { it % 2 == 0 }
 
@@ -11,7 +15,21 @@ fun count(list: ArrayList<Int>): Int? {
         .reduce(0, ) { acc, value -> acc + value }
 }
 
+fun findSingleNumbers(array: Array<Int>): Array<Int> {
+    return Arrays.stream(array)
+        .collect(Collectors.groupingBy(Function.identity()))
+        .values
+        .stream()
+        .filter { it.size == 1 }
+        .flatMap { it.stream() }
+        .collect(Collectors.toList()).toTypedArray()
+}
+
 fun main() {
     println(filtered)
     println(count(ArrayList(list)))
+    findSingleNumbers(arrayOf(1, 2, 2, 3))
+        .forEach {
+            print("$it ") // 1 3
+        }
 }
