bd945f5e0ea58029bbf3f24475bc6a81d21922db
Fortncom <anatoliy.gromov.service@gmail.com>
2024-11-16 12:28:53 +0300

    Замена if на ifEmpty

--- src/main/kotlin/ru/job4j/tracker/action/ShowAllAction.kt
+++ src/main/kotlin/ru/job4j/tracker/action/ShowAllAction.kt
@@ -1,8 +1,6 @@
 package ru.job4j.tracker.action
 
-import ru.job4j.lambda.Account
 import ru.job4j.tracker.Input
-import ru.job4j.tracker.Item
 import ru.job4j.tracker.Tracker
 
 class ShowAllAction : Action {
@@ -11,11 +9,14 @@ class ShowAllAction : Action {
 
     override fun execute(tracker: Tracker, input: Input): Boolean {
         println("=== Show all items ====")
-        val items: List<Item> = tracker.findAll()
-        items.forEach { item: Item? -> println(item) }
-        if (items.isEmpty()) {
-            println("Store is empty")
-        }
+        tracker.findAll()
+            .ifEmpty {
+                println("Store is empty")
+                listOf()
+            }
+            .forEach {
+                println("$it")
+            }
         return true
     }
 }
